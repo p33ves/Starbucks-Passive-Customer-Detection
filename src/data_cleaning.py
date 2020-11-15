a6e7dd21b1db85f5aa5f53162377d4d8e5dcaf8a
Vignesh P <vignesh.p1110@gmail.com>
2020-11-15 21:27:23 +0530

    profile data cleaned

---
+++ src/data_cleaning.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+
+
+transcriptdf = pd.read_json(r'../data/transcript.json', lines=True)
+portfoliodf = pd.read_json(r'../data/portfolio.json', lines=True)
+profiledf = pd.read_json(r'../data/profile.json', lines=True)
+
+profiledf = profiledf[
+    (profiledf['gender'].notnull()) &
+    (profiledf['age'] != 118) &
+    (profiledf['income'].notnull())
+]
+profiledf.rename(columns={'id': 'person'}, inplace=True)
+
+profiledf.to_csv(r'../cleaned_profile.csv')
